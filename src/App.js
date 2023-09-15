84e5b1fa9c4c9ae74bfa8f010feed323156a45ec
Sahil-Karnaje <sahilkaranje58@gmail.com>
2023-09-15 13:00:14 +0530

    Routing done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import Allroutes from './Allroutes';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      Appetite
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Allroutes/>
+      </div>
+    </BrowserRouter>
   );
 }
 
