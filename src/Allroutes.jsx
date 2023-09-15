dae306a90430ec8e35d33c8d620ef1e5d6842f14
Sahil-Karnaje <sahilkaranje58@gmail.com>
2023-09-15 12:49:26 +0530

    lib folder added

--- src/Allroutes.jsx
+++ src/Allroutes.jsx
@@ -1,1 +1,15 @@
-import {}
+import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home/Home';
+
+const Allroutes = () => {
+  return (
+    <div>
+        <Routes>
+            <Route path='/' element={<Home/>} />
+        </Routes>
+    </div>
+  )
+}
+
+export default Allroutes
