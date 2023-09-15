80e77a4a9b5869956d3b6c69a13d67509ed370a5
Sahil-Karnaje <sahilkaranje58@gmail.com>
2023-09-15 12:58:06 +0530

    lib folder connect and allroutes completed

--- src/Allroutes.jsx
+++ src/Allroutes.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import Home from './pages/Home/Home';
+import About from './pages/About/About'
+import Booking from './pages/Booking/Booking'
+import Menu from './pages/Menu/Menu'
+import Login from './pages/Login/Login'
+import Signup from './pages/Signup/Signup'
 
 const Allroutes = () => {
   return (
     <div>
         <Routes>
             <Route path='/' element={<Home/>} />
+            <Route path='/about' element={<About/>} />
+            <Route path='/booking' element={<Booking/>} />
+            <Route path='/menu' element={<Menu/>} />
+            <Route path='/login' element={<Login/>} />
+            <Route path='/signup' element={<Signup/>} />
         </Routes>
     </div>
   )
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
 import './Login.css'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
     <div>
       <div className="container">
         <div className="login_page">
-          
+          <div className="background">
+            <div className="shape"></div>
+            <div className="shape"></div>
+          </div>
+          <form method="POST">
+            <h3>Login Here</h3>
+
+            <label for="username">E-mail</label>
+            <input type="email" placeholder="Email" id="username" name="email" required />
+
+              <label for="password">Password</label>
+              <input type="password" placeholder="Password" id="password" name="password" required />
+
+                <input type="submit" name="submit" className="login_btn" value="Login" />
+                <Link to="/signup" id="singup_link" >create new account</Link>
+              </form>
+            </div>
         </div>
       </div>
-    </div>
-  )
+      )
 }
 
-export default Login
+      export default Login
