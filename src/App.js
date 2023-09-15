0b06c6e2549aceddcf5396e84002fe1aea4596e3
Sahil-Karnaje <sahilkaranje58@gmail.com>
2023-09-15 13:39:40 +0530

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Allroutes from './Allroutes';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Navbar />
         <Allroutes/>
       </div>
     </BrowserRouter>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <div className="container-xxl position-relative p-0">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4 px-lg-5 py-3 py-lg-0">
+          <Link to="/" className="navbar-brand p-0">
+            <h1 className="text-primary m-0"><i className="fa fa-utensils me-3"></i>Appetite</h1>
+            {/* <img src="img/logo.png" alt="Logo" /> */}
+          </Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span className="fa fa-bars"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <div className="navbar-nav ms-auto py-0 pe-4">
+              <Link to="/home" className="nav-item nav-link active">Home</Link>
+              <Link to="/about" className="nav-item nav-link">About</Link>
+              <Link to="/menu" className="nav-item nav-link">Menu</Link>
+              <Link to="/booking" className="nav-item nav-link">Booking</Link>
+              <Link to="/contact" className="nav-item nav-link">Contact</Link>
+            </div>
+            <Link to='/login' class='btn btn-primary py-2 px-4'>Login</Link>
+          </div>
+        </nav>
+      </div>
+    </div>
   )
 }
 
